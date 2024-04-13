3a3ebec290d9df9ac174b7244e00394566a0d2fc
vlespukh <v.lespukh@gmail.com>
2024-04-13 07:00:07 +0200

    start

---
+++ scripts.js
@@ -0,0 +1,114 @@
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+// Examples(Operator, value1, value2) --> output
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+    case '+':
+      return value1 + value2;
+    case '-':
+      return value1 - value2;
+    case '*':
+      return value1 * value2;
+    case '/':
+      return value1 / value2;
+    default:
+      return 'Unknown operation!';
+  }
+}
+
+// Challenge
+// When provided with a number between 0-9, return it in words.
+// Input :: 1
+// Output :: "One".
+
+function switchItUp(number) {
+  switch (number) {
+    case 1:
+      return 'One';
+    case 2:
+      return 'Two';
+    case 3:
+      return 'Three';
+    case 4:
+      return 'Four';
+    case 5:
+      return 'Five';
+    case 6:
+      return 'Six';
+    case 7:
+      return 'Seven';
+    case 8:
+      return 'Eight';
+    case 9:
+      return 'Nine';
+    case 0:
+      return 'Zero';
+    default:
+      return 'Unknown operation.';
+  }
+}
+
+// Challenge
+// Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them.
+
+// a and b will both be positive integers, and a will always be the first number in the operation, and b always the second.
+
+// The four operators are "add", "subtract", "divide", "multiply".
+
+// A few examples:(Input1, Input2, Input3 --> Output)
+
+// 5, 2, "add"      --> 7
+// 5, 2, "subtract" --> 3
+// 5, 2, "multiply" --> 10
+// 5, 2, "divide"   --> 2.5
+
+function arithmetic(a, b, operator) {
+  switch (operator) {
+    case 'add':
+      return a + b;
+    case 'subtract':
+      return a - b;
+    case 'multiply':
+      return a * b;
+    case 'divide':
+      return a / b;
+    default:
+      break;
+  }
+}
+
+// Challenge
+// There was a test in your class and you passed it. Congratulations!
+
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return true if you're better, else false!
+
+// Note:
+// Your points are not included in the array of your class's points. Do not forget them when calculating the average score!
+
+function betterThanAverage(classPoints, yourPoints) {
+  let classAverage = classPoints.reduce((acc, current) => acc + current) / classPoints.length;
+  return classAverage < yourPoints;
+}
+
+// Challenge
+// Objective: Write a function called capitalizeWords that takes a string as input and returns the same string with the first letter of each word capitalized.
+
+function capitalizeWords(words) {
+  return words.split(' ').map((item) => item.charAt(words[0]).toUpperCase() + item.slice(1, item.length)).join(" ");
+}
+
+console.log(capitalizeWords('hello world')); // Output: "Hello World"
+
+// words.charAt(words[0]).toUpperCase() + words.slice(1, words.length)
