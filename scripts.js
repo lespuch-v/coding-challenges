46948a2ec525a8ea5b2df131132f74f85e99ab8c
vlespukh <v.lespukh@gmail.com>
2024-05-04 06:28:12 +0200

    some changes

--- scripts.js
+++ scripts.js
@@ -106,7 +106,10 @@ function betterThanAverage(classPoints, yourPoints) {
 // Objective: Write a function called capitalizeWords that takes a string as input and returns the same string with the first letter of each word capitalized.
 
 function capitalizeWords(words) {
-  return words.split(' ').map((item) => item.charAt(words[0]).toUpperCase() + item.slice(1, item.length)).join(" ");
+  return words
+    .split(' ')
+    .map((item) => item.charAt(words[0]).toUpperCase() + item.slice(1, item.length))
+    .join(' ');
 }
 
 //console.log(capitalizeWords('hello world')); // Output: "Hello World"
@@ -114,46 +117,46 @@ function capitalizeWords(words) {
 // words.charAt(words[0]).toUpperCase() + words.slice(1, words.length)
 
 // Objective: Write a simple script to display "Hello, JavaScript!" in the console.
-function greeting(){
-  return 'Hello, JavaScript!'
+function greeting() {
+  return 'Hello, JavaScript!';
 }
 
 // Objective: Declare two variables, one for your name and one for your age.
 // Display them in a sentence using console.log.
 
-function userInput(name, age){
+function userInput(name, age) {
   return `Hello ${name}, and your age is ${age}.`;
 }
 
 // Objective: Calculate the area of a rectangle. Take the length and width from user input and display the area.
 
-function areaOfRectangle(length, width){
+function areaOfRectangle(length, width) {
   return length * width;
 }
 
 // console.log(areaOfRectangle(8, 3));
 
 // Objective: Write a function that takes a number and returns its square.
-function squareOfNumber(number){
-  return number * number
+function squareOfNumber(number) {
+  return number * number;
 }
 
 // Write a script that checks if a number is even or odd and prints an appropriate message in the console.
-function oddOrEven(number){
-  return number %2 === 0
+function oddOrEven(number) {
+  return number % 2 === 0;
 }
 
 // console.log(oddOrEven(4))
 
 // Objective: Use a loop to print all even numbers from 1 to 20.
-function looping(number){
-  for (let i = 0; i <= number; i++){
-    console.log(i)
+function looping(number) {
+  for (let i = 0; i <= number; i++) {
+    console.log(i);
   }
 }
-function fruits(arrOfFruits){
-  for(let i = 0; i < arrOfFruits.length; i++){
-    console.log(arrOfFruits[i])
+function fruits(arrOfFruits) {
+  for (let i = 0; i < arrOfFruits.length; i++) {
+    console.log(arrOfFruits[i]);
   }
 }
 
@@ -162,25 +165,53 @@ function fruits(arrOfFruits){
 // Objective: Create an object representing a car with properties like make, model, and color. Display each property.
 let car = [
   {
-    'key': 'a',
-    'test': 'b'
-  }
-]
-function items(obj){
-  for (let i = 0; obj.length;i++){
-    console.log(car)
+    key: 'a',
+    test: 'b',
+  },
+];
+function items(obj) {
+  for (let i = 0; obj.length; i++) {
+    console.log(car);
   }
 }
 
-function palindrome(word){
-  let forward = word.split('')
-  let backwards = word.split('').reverse()
+function palindrome(word) {
+  let forward = word.split('');
+  let backwards = word.split('').reverse();
 
-  for(let i = 0; forward.length > i;i++){
-    if(forward[i] !== backwards[i]){
-      return false
+  for (let i = 0; forward.length > i; i++) {
+    if (forward[i] !== backwards[i]) {
+      return false;
     }
   }
   return true;
 }
+
+let myNumbers = [1, 2, 3, 4];
+const newNumbers = myNumbers.map((item) => item * item);
+
+let someNumber = [1, 2, 3, 4, 5, 6];
+const evenNumbers = someNumber.filter((item) => item % 2 !== 0);
+
+const greaterThan = evenNumbers.map((item) => item * 2).filter((n) => n > 4);
+
+const n2 = [-1, 2, 3, -4, 5];
+const n2ResultSum = n2.filter((n) => n > 0).reduce((a, b) => a + b);
+
+n3 = ['apple', 'banana', 'grapefruit', 'kiwi'];
+const n3Words = n3.reduce((a, b) => {
+  return a.length > b.length ? a : b;
+});
+
+const duplicates = [1, 2, 2, 3, 4, 4, 5];
+
+const removedDuplicates = duplicates.filter((value, index, self) => {
+  return index === self.indexOf(value);
+});
+
+const names = ['alice', 'bob', 'carol'];
+const upperNames = names.map((item) => item.substring(0, 1).toUpperCase() + item.substring(1, item.length));
+console.log(upperNames)
+
+const sortedValues = removedDuplicates.sort();
 
